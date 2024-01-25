038975f645b1e6cff94ff63991121aa1b18027b5
Thomas Kim <tkimm156@gmail.com>
2024-01-24 20:47:46 -0600

    Restore functionality to form

--- memegen.js
+++ memegen.js
@@ -1,17 +1,79 @@
-const form = document.querySelector(".form");
+const newMemeBtn = document.querySelector(".new-meme");
+
 let formData;
 
-form.addEventListener("submit", event => {
-  event.preventDefault();
-  formData = new FormData(form);
-  const url = formData.get("url");
-  const top = formData.get("top");
-  const bottom = formData.get("bottom");
-  form.reset();
-  makeMeme(url, top, bottom);
-});
-
-function makeMeme(url, top, bottom) {
-  console.log(url + " " + top + " " + bottom);
+function makeForm() {
+  const create = document.querySelector(".create");
+  const formContainer = document.createElement("div");
+  formContainer.setAttribute("class", "form-container");
+  const formEle = document.createElement("form");
+  formEle.setAttribute("class", "form");
+  //create url section
+  const urlDiv = document.createElement("div");
+  const urlLabel = document.createElement("label");
+  urlLabel.setAttribute("for", "url");
+  urlLabel.textContent = "Image URL:";
+  const urlInput = document.createElement("input");
+  urlInput.setAttribute("id", "url");
+  urlInput.setAttribute("type", "url")
+  urlInput.setAttribute("name", "url");
+  urlInput.setAttribute("required", "true");
+  urlInput.setAttribute("placeholder", "Paste image adress here");
+  urlDiv.appendChild(urlLabel);
+  urlDiv.appendChild(urlInput);
+  formEle.appendChild(urlDiv);
+  //create top text section
+  const topDiv = document.createElement("div");
+  const topLabel = document.createElement("label");
+  topLabel.setAttribute("for", "top");
+  topLabel.textContent = "Top Text:";
+  const topInput = document.createElement("input");
+  topInput.setAttribute("id", "top");
+  topInput.setAttribute("name", "top");
+  topInput.setAttribute("placeholder", "TOP TEXT");
+  topDiv.appendChild(topLabel);
+  topDiv.appendChild(topInput);
+  formEle.appendChild(topDiv);
+  //create bottom text section
+  const botDiv = document.createElement("div");
+  const botLabel = document.createElement("label");
+  botLabel.setAttribute("for", "bot");
+  botLabel.textContent = "Bottom Text:";
+  const botInput = document.createElement("input");
+  botInput.setAttribute("id", "bot");
+  botInput.setAttribute("name", "bot");
+  botInput.setAttribute("placeholder", "BOTTOM TEXT");
+  botDiv.appendChild(botLabel);
+  botDiv.appendChild(botInput);
+  formEle.appendChild(botDiv);
+  //create subbmission
+  const submit = document.createElement("div");
+  submit.setAttribute("class", "submit");
+  const submitInput = document.createElement("input");
+  submitInput.setAttribute("type", "submit");
+  submitInput.setAttribute("value", "Memeify!");
+  submit.appendChild(submitInput);
+  //append it all
+  formEle.appendChild(submit);
+  formContainer.appendChild(formEle);
+  create.appendChild(formContainer);
 }
 
+newMemeBtn.addEventListener("click", event => {
+  newMemeBtn.style.display = "none";
+  makeForm();
+  const form = document.querySelector(".form");
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    formData = new FormData(form);
+    const urlData = formData.get("url");
+    const topData = formData.get("top");
+    const botData = formData.get("bot");
+    form.reset();
+    makeMeme(urlData, topData, botData);
+  });
+});
+
+function makeMeme() {
+  console.log(formData);
+}
